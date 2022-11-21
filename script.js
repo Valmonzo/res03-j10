352ca1b4a2763b4e682a0455f183fa22ab818fd2
Valmonzo <valmontpehautpietri@ip-172-31-16-115.eu-west-3.compute.internal>
2022-11-21 15:11:38 +0000

    Mise à jour du Js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let formulaire1 = document.getElementById("form1");
+/*let formulaire1 = document.getElementById("form1");
 let formulaire2 = document.getElementById("form2");
 let formulaire3 = document.getElementById("form3");
 
@@ -21,5 +21,22 @@ formulaire3.addEventListener("click", function(){
     formulaire1.style.opacity = 0.35;
     formulaire2.style.opacity = 0.35;
 
-})
+}) */
+
+let section = document.querySelectorAll("body > main > section");
+let fakeBtn = document.querySelectorAll("body > main > section > .fakeBtn");
+
+for (let i = 0; i < section.length; i++) {
+    fakeBtn[i].addEventListener("click", function(){
+        
+        if((i+1) < section.length){
+            section[i].classList.remove("active");
+            section[i+1].classList.add("active");
+        }else{
+            section[i].classList.remove("active");
+            section[0].classList.add("active");
+
+        }
+    })    
+}
 
